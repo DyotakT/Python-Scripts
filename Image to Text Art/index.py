ec5d5bc9df9f7bfb1dfdbbbcca4d6bbb4e4fd148
Dyotak Thengdi <dyotakt@gmail.com>
2021-09-23 21:50:17 +0530

    Optimization and Bug Fixes
    Optimized code - now normalizes and finds the index to be used in string of elements instead of using multiple if-else conditions.
    Added comments to explain what is happening.
    Fixed and optimized darkBackground condition, now working.
    Added example image.
    Added README.md file.

--- Image to Text Art/index.py
+++ Image to Text Art/index.py
@@ -1,49 +1,31 @@
 from PIL import Image, ImageOps
-import sys
+import sys, math
 
-img = Image.open(r"image.png")
+img = Image.open(r"image.png") # Image to be used path
+elements = " .:cx0@" # Elements to be used; from least dense to most dense
+darkBackground = sys.argv[1] # Get if the background is dark or light from first command line argument
+charLength = sys.argv[2] # Get the characters length of art from second command line argument
+charLength = int(charLength)
 
-darkBackground = sys.argv[1]
+if(int(darkBackground)==0):
+    elements = elements[::-1] # Reverse the elements string if background is light
 
-smolimg = img.resize((50,50)) # Resizing image to make it small
+smolimg = img.resize((charLength,charLength)) # Resizing image to make it small
 greypix = ImageOps.grayscale(smolimg) # Turning image into greyscale
 pix_val_grey = list(greypix.getdata())
 
 # greypix.show() # Use this to see the grey and small image that will be used
 
-width, height = smolimg.size
+width, height = smolimg.size # Getting height and width of the resized image
+pex = [[0 for x in range(width)] for y in range(height)] # Declaring a 2D Array
+definition = len(elements) # Get the number of elements being used for future step of normalization
 
-pex = [[0 for x in range(width)] for y in range(height)] 
-
-count = 0 # To Count 
-heightcount = 0
+count = 0 # Used to traverse the pix_val_grey array 
 for x in range(height):
     for y in range(width):
-        pex[x][y]=pix_val_grey[count]
-        if(darkBackground==1):
-            if(pex[x][y]>204):
-                pex[x][y]=" "
-            elif(pex[x][y]>153 and pex[x][y]<205):
-                pex[x][y]="."
-            elif(pex[x][y]>102 and pex[x][y]<154):
-                pex[x][y]="x"
-            elif(pex[x][y]>51 and pex[x][y]<103):
-                pex[x][y]="0"
-            elif(pex[x][y]<52):
-                pex[x][y]="@"
-        else:
-            if(pex[x][y]>204):
-                pex[x][y]="@"
-            elif(pex[x][y]>53 and pex[x][y]<205):
-                pex[x][y]="0"
-            elif(pex[x][y]>102 and pex[x][y]<154):
-                pex[x][y]="x"
-            elif(pex[x][y]>51 and pex[x][y]<103):
-                pex[x][y]="."
-            elif(pex[x][y]<52):
-                pex[x][y]=" "
+        pex[x][y]=pix_val_grey[count] # Traversing pix_val_grey and copying value into the array
+        pex[x][y] = elements[math.ceil((pex[x][y]/255)*definition)-1] # Picking the element at index of the normalized value
         count=count+1
-    heightcount=heightcount+1
 
 for x in range(height):
     print(*pex[x], sep="") # Printing the art into terminal
